6bd5fb8629d4d6a76f5983f2691a2129b268c6ce
kyle <kwhershey@gmail.com>
2018-08-25 11:52:42 -0500

    editing so exports eps for peter

--- peter/strava_bulk_plot.py
+++ peter/strava_bulk_plot.py
@@ -13,10 +13,11 @@ data = [f for f in listdir(data_path) if isfile(join(data_path,f))]
 lat = []
 lon = []
 
-fig = plt.figure(facecolor = '0.05')
+fig = plt.figure(facecolor = 'black')
 ax = plt.Axes(fig, [0., 0., 1., 1.], )
 ax.set_aspect('equal')
 ax.set_axis_off()
+#ax.set_facecolor('black')
 fig.add_axes(ax)
 
 for activity in data:
@@ -31,15 +32,16 @@ for activity in data:
                 for point in segment.points:
                     lat.append(point.latitude)
                     lon.append(point.longitude)
-        if abs(lat[0]-44.9)<1 and abs(lon[0]+93.2)<1:
+        if abs(lat[0]-44.9)<1 and abs(lon[0]+93.2)<1: # MSP
+        #if abs(lat[0]-41.5)<1 and abs(lon[0]+81.7)<1: # CLE 
             plt.plot(lon, lat, color = 'deepskyblue', lw = 0.4, alpha = 0.2)
         lat = []
         lon = []
 
 #plt.scatter(-93.1954600,44.9665240,color = 'deepskyblue',alpha=1)
 
-filename = data_path + '.png'
-plt.savefig(filename, facecolor = fig.get_facecolor(), bbox_inches='tight', pad_inches=0, dpi=300)
+filename = data_path + '.eps'
+plt.savefig(filename, facecolor='black', bbox_inches='tight', pad_inches=0)#, dpi=300)
 
 plt.show()
 
