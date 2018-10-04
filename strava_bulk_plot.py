266d798f2f671db4d515303b8a8d9dbb53bbd402
kyle <kwhershey@gmail.com>
2018-10-03 23:37:57 -0500

    elevation plots

--- strava_bulk_plot.py
+++ strava_bulk_plot.py
@@ -1,7 +1,15 @@
 from os import listdir
 from os.path import isfile, join
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import gpxpy
+import time
+
+tic=time.clock()
+
+home_lat=47.5
+home_long=-122
 
 data_path = 'activities'
 data = [f for f in listdir(data_path) if isfile(join(data_path,f))]
@@ -9,30 +17,34 @@ data = [f for f in listdir(data_path) if isfile(join(data_path,f))]
 lat = []
 lon = []
 
-fig = plt.figure(facecolor = '0.05')
+fig = plt.figure(facecolor = 'black')
 ax = plt.Axes(fig, [0., 0., 1., 1.], )
 ax.set_aspect('equal')
 ax.set_axis_off()
 fig.add_axes(ax)
 
 for activity in data:
-    gpx_filename = join(data_path,activity)
-    gpx_file = open(gpx_filename, 'r')
-    gpx = gpxpy.parse(gpx_file)
+    if activity[-1] == 'x':
+        gpx_filename = join(data_path,activity)
+        gpx_file = open(gpx_filename, 'r')
+        gpx = gpxpy.parse(gpx_file)
 
-    for track in gpx.tracks:
-        for segment in track.segments:
-            for point in segment.points:
-                lat.append(point.latitude)
-                lon.append(point.longitude)
-    plt.plot(lon, lat, color = 'deepskyblue', lw = 0.8, alpha = 0.4)
-    lat = []
-    lon = []
+        for track in gpx.tracks:
+            for segment in track.segments:
+                for point in segment.points:
+                    lat.append(point.latitude)
+                    lon.append(point.longitude)
+        if abs(lat[0]-home_lat)<10 and abs(lon[0]-home_long)<10:  
+            plt.plot(lon, lat, color = 'deepskyblue', lw = 0.8, alpha = 0.4)
+        lat = []
+        lon = []
 
-plt.scatter(-93.1954600,44.9665240,color = 'deepskyblue',alpha=1)
 
 filename = data_path + '.png'
 plt.savefig(filename, facecolor = fig.get_facecolor(), bbox_inches='tight', pad_inches=0, dpi=300)
+filename = data_path + '.eps'
+plt.savefig(filename, facecolor='black', bbox_inches='tight', pad_inches=0)#, dpi=300)
 
-plt.show()
 
+toc=time.clock()
+print(toc-tic)
